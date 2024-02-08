749019a346d4e18feb8ca9fa806103784c03fe80
ALEEZAJOGIYAT <alizajogyat09@gmail.com>
2024-02-08 22:30:16 +0500

    created server and client routes

---
+++ src/components/client.js
@@ -0,0 +1,13 @@
+'use client'
+
+
+const ClientComponent = () => {
+    return(
+        <div>
+            <h2> I am client render compoennt</h2>
+        </div>
+    )
+}
+
+
+export default ClientComponent;
---
+++ src/components/server.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export async function ServerComponent (){
+    return(
+        <div>
+            Hello I am server side rendered component ':))))'
+        </div>
+    )
+
+}
